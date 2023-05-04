26b46d0086bf74e2d83c820970f18b269a074cb9
masenn <masenn@dhs.dominicanhighschool.com>
2023-05-04 09:23:40 -0500

    added spinning kabba

--- sketch.js
+++ sketch.js
@@ -1,7 +1,58 @@
 function setup(){
-    createCanvas(700,500);
+    createCanvas(800,500, WEBGL);
+    fArial = loadFont('/fonts/arial.ttf')
+    textFont(fArial);
+    im = loadImage("imgs/k.png");
 }
 
+
+function keyPressed(){
+    if(keyCode == 87){
+        tr.x += -sin(theta)*sensitivity;
+        tr.z += -cos(theta)*sensitivity;
+        console.log('w')
+    } else if (keyCode == 83){
+        tr.x += sin(theta)*sensitivity;
+        tr.z += cos(theta)*sensitivity;
+    }
+}
+
+
+mousePrev = {
+    x:0,
+    y:0
+}
+
+theta = 0;
+
+sensitivity = 7;
+tr = {
+    x:0,
+    y:0,
+    z:0
+}
+
+let im;
+c = 0
+
 function draw(){
-    circle(50,50,30);
+    background(127);
+    fill(255,0,0);
+    // orbitControl();
+    theta += (mouseX - mousePrev.x)/width*360;
+    text(theta, 50,50)
+    mousePrev.x = mouseX;
+    push();
+        translate(tr.x, tr.y, tr.z);
+        circle(0,0,30);
+        // image(im,30,30);
+        texture(im);
+        rotateY(c);
+        c+=.1;
+        box(90);
+    pop();
+
+    // camera(
+
+    // ); 
 }

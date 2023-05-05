0a0e388f07a8e9d72db9fcf3e9a3d24471da9d1e
masenn <masenn@dhs.dominicanhighschool.com>
2023-05-05 08:01:59 -0500

    update

--- sketch.js
+++ sketch.js
@@ -5,15 +5,50 @@ function setup(){
     im = loadImage("imgs/k.png");
 }
 
+//            w      s      a      d      UP     DOWN
+const keys = [false, false, false, false, false, false];
 
 function keyPressed(){
     if(keyCode == 87){
-        tr.x += -sin(theta)*sensitivity;
-        tr.z += -cos(theta)*sensitivity;
-        console.log('w')
+        //w
+        keys[0] = true;
     } else if (keyCode == 83){
-        tr.x += sin(theta)*sensitivity;
-        tr.z += cos(theta)*sensitivity;
+        //s
+        keys[1] = true;
+    }else if (keyCode == 65){
+        //a
+        keys[2] = true;
+    }else if (keyCode == 68){
+        //d
+        keys[3] = true;
+    } else if(keyCode == 38){
+        //UP
+        keys[4] = true;
+    } else if(keyCode == 40){
+        //DOWN
+        keys[5] = true;
+    }
+}
+
+function keyReleased(){
+    if(keyCode == 87){
+        //w
+        keys[0] = false;
+    } else if (keyCode == 83){
+        //s
+        keys[1] = false;
+    }else if (keyCode == 65){
+        //a
+        keys[2] = false;
+    }else if (keyCode == 68){
+        //d
+        keys[3] = false;
+    } else if(keyCode == 38){
+        //UP
+        keys[4] = false;
+    } else if(keyCode == 40){
+        //DOWN
+        keys[5] = false;
     }
 }
 
@@ -24,8 +59,9 @@ mousePrev = {
 }
 
 theta = 0;
+phi = 0; 
 
-sensitivity = 30;
+sensitivity = 15;
 tr = {
     x:0,
     y:0,
@@ -33,28 +69,64 @@ tr = {
 }
 
 let im;
-c = 0
+c = 0;
+
 
 function draw(){
-    background(127);
+    background(90);
+
+    for(let i = 0; i < keys.length; i++){
+        if(keys[i]){
+            switch (i){
+
+                case 0:
+                    //w
+                    tr.x += -sin(theta)*sensitivity;
+                    tr.z += -cos(theta)*sensitivity;
+                    break;
+                case 1: 
+                    tr.x += sin(theta)*sensitivity;
+                    tr.z += cos(theta)*sensitivity;
+                    break;
+                case 2: 
+                    tr.x += -sin(theta-PI/2)*sensitivity;
+                    tr.z += -cos(theta-PI/2)*sensitivity;
+                    break;
+                case 3: 
+                    tr.x += -sin(theta-PI/2)*sensitivity;
+                    tr.z += -cos(theta-PI/2)*sensitivity;
+                case 4:
+                    tr.y -= sensitivity;
+                    break;
+                case 5: 
+                    tr.y += sensitivity;
+                    break;
+            }
+        }
+    }
+
     fill(255,0,0);
-    // orbitControl();
-    theta += (mouseX - mousePrev.x)/width*360;
-    text(theta, 50,50)
+
+    theta += (mouseX - mousePrev.x)*360/width*PI/180;
     mousePrev.x = mouseX;
+
+    phi += (mouseY - mousePrev.y)*180/height*PI/180;
+    mousePrev.y = mouseY;
+
+    stroke(255,255,255);
     push();
         translate(tr.x, tr.y, tr.z);
-        circle(0,0,30);
+        // circle(0,0,30);
         // image(im,30,30);
-        // texture(im);
-        rotateY(c);
-        c+=.1;
+        texture(im);
+        // rotateY(c);
+        // c+=.1;
         box(90);
     pop();
 
     camera(
         0,0,0,
-        sin(theta),0,cos(theta),
+        sin(theta),sin(phi),cos(theta),
         0,-1,0
     ); 
 }
